1684dac48bfbddd03c76ec60663663a8ffaa56a5
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-06-30 16:57:38 -0700

    fixed adding new post

--- script.js
+++ script.js
@@ -118,7 +118,27 @@ function init (headers, article){
   }
 
 }
+blogPost.addEventListener('click', (e)=> {
+  addNewPost(e);
+})
+
 
 blogPost.addEventListener('input', editText);
 init(headers, article);
 
+
+headers.forEach((headers) => {
+  addIntro(headers.logo, headers.title, headers.logoAlt);
+})
+article.forEach((article) => {
+  addContent(article.title, article.text);
+})
+
+//по кнопке добавляет карточку
+
+
+function addNewPost(event) {
+  if(event.target.classList.contains('blog-post__icon-heading')){
+    addContent('Заголовок', 'Текст')
+  }
+}
